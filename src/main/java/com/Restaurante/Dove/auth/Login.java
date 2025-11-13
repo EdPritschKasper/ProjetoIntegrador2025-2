3c2a55fa4ab4226560e1e84a236e99cf1986e212
Eduardo Henrique Fritsche <edu.fritsche@gmail.com>
2025-11-13 13:03:23 -0300

    Add arquivos basicos para autenticação e login

---
+++ src/main/java/com/Restaurante/Dove/auth/Login.java
@@ -0,0 +1,13 @@
+package com.Restaurante.Dove.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Login extends Usuario {
+
+	private String username;
+	private String password;
+	
+}
