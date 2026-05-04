348a01c14be20f2effd19c08571c9bea629d2179
Lethicia <lethiciamsm@gmail.com>
2026-05-03 21:00:31 -0300

    dependencia para o tomcat funcionar

--- src/main/java/com/Restaurante/Dove/DoveApplication.java
+++ src/main/java/com/Restaurante/Dove/DoveApplication.java
@@ -2,9 +2,16 @@ package com.Restaurante.Dove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DoveApplication {
+public class DoveApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DoveApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoveApplication.class, args);
